ac4ae60b0225fa72ef24e688cdb83b89b4a957a3
Elena Afanasova <eafanasova@gmail.com>
2020-12-26 20:47:00 +0300

    KVM: add KVM_CAP_IOREGIONFD
    Signed-off-by: Elena Afanasova <eafanasova@gmail.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4161,6 +4161,7 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 	case KVM_CAP_SREGS2:
 	case KVM_CAP_EXIT_ON_EMULATION_FAILURE:
 	case KVM_CAP_VCPU_ATTRIBUTES:
+	case KVM_CAP_IOREGIONFD:
 		r = 1;
 		break;
 	case KVM_CAP_EXIT_HYPERCALL:
